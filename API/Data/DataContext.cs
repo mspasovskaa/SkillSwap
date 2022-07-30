107e99733aa794f73787b8a02a3ecac439384325
Marija <94226130+mspasovskaa@users.noreply.github.com>
2022-07-30 19:41:45 +0200

    First commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using API.Entities;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<AppUser> Users { get; set; }
+
+    }
+}
